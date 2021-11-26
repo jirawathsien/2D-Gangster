df7ec9fc964bde6f8bbdd6b2d1dbf9d508ec03e6
KhizarFarooqGameDev <juniorhsien@gmail.com>
2021-11-26 17:31:36 +0500

    update

--- 2D RPG GAME/Assets/ShowArrow.cs
+++ 2D RPG GAME/Assets/ShowArrow.cs
@@ -5,16 +5,16 @@ using UnityEngine.UI;
 
 public class ShowArrow : MonoBehaviour
 {
-   public Image arrow;
+   public CanvasGroup arrow;
    
    private void OnTriggerEnter2D(Collider2D other)
    {
       if (other.gameObject.CompareTag("Player"))
       {
          arrow.gameObject.SetActive(true);
-         arrow.DOFade(1f, 2f).OnComplete(() =>
+         arrow.DOFade(1f, 1f).OnComplete(() =>
          {
-            arrow.DOFade(0f, 2f).OnComplete(() =>
+            arrow.DOFade(0f, 5f).OnComplete(() =>
             {
                arrow.gameObject.SetActive(false);
             });
